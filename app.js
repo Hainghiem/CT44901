cb23be0b8f70286bee50ed6326e67f2e101ff046
Hainghiem <nghiemb2100111@student.ctu.edu.vn>
2024-09-17 21:44:36 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
-const conractsRouter = require("./app/routes/contact.route")
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req, res) => {
-    res.json({ message: "welcome to contact book application." });
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
 });
 
-app.use("/api/contacts",conractsRouter);
+// Định nghĩa route cho contacts
+app.use("/api/contacts", contactsRouter);
+
+// Xử lý lỗi 404 cho các route không tồn tại
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error", // Sử dụng 'err' thay vì 'error'
+    });
+});
 
 module.exports = app;
