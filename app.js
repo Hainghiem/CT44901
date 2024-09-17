c658e88ec4dd5cb6257fafb3068fea7d382d1328
Hainghiem <nghiemb2100111@student.ctu.edu.vn>
2024-09-17 20:55:56 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const conractsRouter = require("./app/routes/contact.route")
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/",(req, res) => {
     res.json({ message: "welcome to contact book application." });
 });
+
+app.use("/api/contacts",conractsRouter);
 
 module.exports = app;
